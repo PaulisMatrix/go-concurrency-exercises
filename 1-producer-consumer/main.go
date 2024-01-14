77aa3eb32793264649c5cece39abdb84e97c022b
PaulisMatrix <1999.yadwade@gmail.com>
2024-01-14 15:04:08 +0530

    exercise1 complete

--- 1-producer-consumer/main.go
+++ 1-producer-consumer/main.go
@@ -10,39 +10,74 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
-func producer(stream Stream) (tweets []*Tweet) {
+func producer(stream Stream, tweetChan chan<- *Tweet, wg *sync.WaitGroup) {
+	defer wg.Done()
+
 	for {
 		tweet, err := stream.Next()
 		if err == ErrEOF {
-			return tweets
+			close(tweetChan)
+			return
+			//return tweets
 		}
-
-		tweets = append(tweets, tweet)
+		tweetChan <- tweet
+		//tweets = append(tweets, tweet)
 	}
 }
 
-func consumer(tweets []*Tweet) {
-	for _, t := range tweets {
-		if t.IsTalkingAboutGo() {
-			fmt.Println(t.Username, "\ttweets about golang")
-		} else {
-			fmt.Println(t.Username, "\tdoes not tweet about golang")
+func consumer(tweetChan <-chan *Tweet, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for {
+		select {
+		case t, ok := <-tweetChan:
+			if !ok {
+				fmt.Println("channel closed. no more tweets. returning!")
+				return
+			}
+			if t.IsTalkingAboutGo() {
+				fmt.Println(t.Username, "\ttweets about golang")
+			} else {
+				fmt.Println(t.Username, "\tdoes not tweet about golang")
+			}
+
 		}
 	}
+
+	/*
+		for _, t := range tweets {
+			if t.IsTalkingAboutGo() {
+				fmt.Println(t.Username, "\ttweets about golang")
+			} else {
+				fmt.Println(t.Username, "\tdoes not tweet about golang")
+			}
+		}
+	*/
 }
 
 func main() {
+	var wg sync.WaitGroup
+
 	start := time.Now()
 	stream := GetMockStream()
 
+	// use a chan for producer to add tweet to it and consumer to consume from
+
+	tweetChan := make(chan *Tweet)
+
 	// Producer
-	tweets := producer(stream)
+	wg.Add(1)
+	go producer(stream, tweetChan, &wg)
 
 	// Consumer
-	consumer(tweets)
+	wg.Add(1)
+	go consumer(tweetChan, &wg)
 
+	wg.Wait()
 	fmt.Printf("Process took %s\n", time.Since(start))
+
 }
